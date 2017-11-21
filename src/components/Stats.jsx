6f47cc853715313412ec420def86282b5cfcdb5e
prudywsh <antoineprudhomme5@gmail.com>
2017-11-21 15:14:48 +0100

    sales summary table added

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -10,7 +10,9 @@ class Stats extends React.Component {
 
     this.state = {
       orders: null,
-      sortedOrders: null
+      sortedOrders: null,
+      ordersCounters: null,
+      prices: null,
     };
 
     this.onOrdersFetch = this.onOrdersFetch.bind(this);
@@ -29,7 +31,10 @@ class Stats extends React.Component {
    */
   onOrdersFetch(orders) {
     const sortedOrders = new Map();
+    const ordersCounters = new Map();
+    const prices = new Map();
 
+    // fill the sorted orders Map
     orders
     // remove the orders useless for stats
     .filter(order => order.statsCategory)
@@ -47,27 +52,60 @@ class Stats extends React.Component {
       );
     })
 
-    this.setState({orders, sortedOrders});
+    // fill the counters Map
+    orders
+    .map(order => ordersCounters.set(order.name, (ordersCounters.get(order.name) || 0) + 1))
+
+    // fill the prices Map
+    orders
+    .map(order => !prices.has(order.name) && prices.set(order.name, order.price))
+
+    this.setState({orders, sortedOrders, ordersCounters, prices});
   }
 
   render() {
 
+    // generate charts
     const charts = [];
-
     this.state.sortedOrders && this.state.sortedOrders
     .forEach((items, category) => {
       charts.push(<PieChart name={category} items={items} />)
     })
 
+    // generate table rows
+    const rows = [];
+    this.state.ordersCounters && this.state.ordersCounters
+    .forEach((counter, name) => {
+      rows.push(<tr>
+                  <td>{name}</td>
+                  <td>{counter}</td>
+                  <td>{`${(this.state.prices.get(name) * counter) / 100}`}</td>
+                </tr>)
+    })
+
     return (
-      <div>
+      <div className="b-stats">
         <div className="b-stats__title">
           <Link to="/" className="b-sell__title__back">&lsaquo;</Link>
           <span>
             Stats
           </span>
         </div>
-        {charts}
+        <div className="b-stats__table__container">
+          <h2>Résumé des ventes</h2>
+          <table className="b-stats__table">
+            <tr>
+              <th>Item</th>
+              <th>Vendu</th>
+              <th>Montant</th>
+            </tr>
+            {rows}
+          </table>
+        </div>
+        <div>
+          <h2>Graphiques</h2>
+          {charts}
+        </div>
       </div>
     );
   }
