ddbba404a164dc79be1c92c6c76e37e1b838b92a
prudywsh <antoineprudhomme5@gmail.com>
2017-11-21 09:19:17 +0100

    the pie charts are now displaying our data + some style conf

--- src/components/PieChart.jsx
+++ src/components/PieChart.jsx
@@ -12,13 +12,32 @@ export default class PieChart extends React.Component {
     const myPieChart = new Chart(ctx,{
       type: 'pie',
       data: {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        labels: Array.from(this.props.items.keys()),
         datasets: [{
-          label: "My First dataset",
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45],
+            backgroundColor: [
+              'rgba(255,99,132,1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+            ],
+            data: Array.from(this.props.items.values())
          }]
+      },
+      options: {
+        title: {
+            display: true,
+            text: this.props.name,
+            fontSize: 32
+        },
+        legend: {
+            display: true,
+            position: 'bottom',
+            labels: {
+              fontSize: 18
+            }
+        }
       }
     });
   }
