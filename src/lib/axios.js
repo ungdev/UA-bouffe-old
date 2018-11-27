7313f38e7fe0096f6ff5928910dce7d929490f97
Arnaud9145 <arnaud.dufour@utt.fr>
2018-11-27 14:51:13 +0100

    added protocol

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 import { config } from '../../config/config'
 
 export default axios.create({
-  baseURL: `http://${config.api}`
+  baseURL: `${config.protocol}://${config.api}`
 })
